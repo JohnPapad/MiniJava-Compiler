5cb307b303a13da38025ade971b7cc3c797b4744
John Pap <sdi1400144@di.uoa.gr>
2020-06-16 23:59:51 +0300

    Moved and renamed testInputs script
    also made some improvements

---
+++ src/testOutputs.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'         
+NC='\033[0m' # No Color
+
+INPUT_FOLDER="./Tests"
+
+#if there are no java files exit
+if [ $(find $INPUT_FOLDER -maxdepth 1 -type f -name '*.java' -printf x | wc -c) = 0 ]; then
+    echo -e "${RED}-ERROR:${NC} No input files found"
+    exit 0
+fi
+
+for filename in $INPUT_FOLDER/*.java; do
+    echo -----------------------------------------
+    echo $filename
+
+    purefn=$(basename "$filename" .java)
+    echo --- Now testing input file: "$purefn" ---
+
+    # check if the corresponding LLVM file has been generated
+    if [ $(find $INPUT_FOLDER -maxdepth 1 -type f -name "$purefn.ll" -printf x | wc -c) = 0 ]; then
+        echo -e "${RED}-ERROR:${NC} $purefn.ll not found"
+        continue
+    fi
+
+    # add the 'public' keyword at the start of the java file (if not present)
+    first_word=$(awk '{print $1; exit}' $filename)
+    if [ $first_word != "public" ]; then 
+        sed -i '1s/^/public /' $filename 
+    fi
+
+
+    echo - Java Compiling "$purefn.java" ...
+    javac "$filename"
+
+    echo - Java Executing "$purefn" ...
+    # java "Tests/$purefn" > "$purefn".java.out
+    java -cp $INPUT_FOLDER $purefn > "$INPUT_FOLDER/$purefn".java.out
+
+    # check for execution errors
+
+    echo - LLVM Compiling "$purefn".ll ...
+    clang-4.0 -Wno-override-module -o "$INPUT_FOLDER/$purefn".ll.exe "$INPUT_FOLDER/$purefn".ll
+    echo - Executing "$purefn".ll ...
+    "$INPUT_FOLDER/$purefn".ll.exe > "$INPUT_FOLDER/$purefn".ll.out
+
+    cmp --silent "$INPUT_FOLDER/$purefn".java.out "$INPUT_FOLDER/$purefn".ll.out && echo -e "${GREEN}### SUCCESS ###${NC}" || echo -e "${RED}### FAILURE ###${NC}"    
+    # rm -f *.class *~
+    # rm -f *.out *~
+    # rm -f *.exe *~
+    
+    
+done
+
+rm -f "$INPUT_FOLDER/"*.class "$INPUT_FOLDER/"*.exe "$INPUT_FOLDER/"*~
+
+
